5ae799c9ad46d349f521fdab6ceb2089a8df95d9
Serkan Kibritoglu <kirpit@gmail.com>
2012-06-09 16:22:41 +1000

    web2py global vars and helpers

--- README.md
+++ README.md
@@ -12,19 +12,19 @@ Place this file under your web2py applications/myapp/modules.
 
 from applications.myapp.modules.web2jinja import Web2Jinja
 
-@Web2Jinja(request)
+@Web2Jinja(locals())
 def index():
     return {
         'foo': 'bar',
     }
 ```
 
-will render same view file i.e. applications/myapp/views/somecontroller/index.html
-by using Jinja2!
+will render same view file i.e. applications/myapp/views/somecontroller/index.html <br>
+by using Jinja2! It will include all the global variables such as request, <br>
+response, session, cache and other helpers but only "ON" boolean helper will be missing.
 
 License
 =======
-The MIT License (MIT)
-Copyright (c) <2012> <Roy Enjoy a.k.a. kirpit>
+The MIT License (MIT)<br>
+Copyright (c) 2012 Roy Enjoy a.k.a. kirpit<br>
 http://www.opensource.org/licenses/mit-license.php
-
--- web2jinja.py
+++ web2jinja.py
@@ -3,9 +3,13 @@
 <web2jinja.py>
 https://github.com/kirpit/web2jinja
 
+web2jinja
+=========
+
 web2py controller decorator that returns Jinja2 compiled template string.
 
-Example usage:
+Usage
+=====
 Place this file under your web2py applications/myapp/modules.
 
 ```python
@@ -13,30 +17,37 @@ Place this file under your web2py applications/myapp/modules.
 
 from applications.myapp.modules.web2jinja import Web2Jinja
 
-@Web2Jinja(request)
+@Web2Jinja(locals())
 def index():
     return {
         'foo': 'bar',
     }
 ```
 
-will render same view file i.e. applications/myapp/views/somecontroller/index.html
-by using Jinja2!
+will render same view file i.e. applications/myapp/views/somecontroller/index.html <br>
+by using Jinja2! It will include all the global variables such as request, <br>
+response, session, cache and other helpers but only "ON" boolean helper will be missing.
 
-==================================================
-The MIT License (MIT)
-Copyright (c) <2012> <Roy Enjoy a.k.a. kirpit>
+License
+=======
+The MIT License (MIT)<br>
+Copyright (c) 2012 Roy Enjoy a.k.a. kirpit<br>
 http://www.opensource.org/licenses/mit-license.php
-==================================================
 """
 from jinja2 import Environment, PackageLoader
 
 
 class Web2Jinja(object):
+    web2py_locals = None
     template = None
-    func = None
 
-    def __init__(self, request):
+    def __init__(self, web2py_locals):
+        # filter web2py globals
+        self.web2py_locals = dict([(k, v) for k, v in web2py_locals.iteritems()
+                                          if not k.startswith('_') and
+                                             getattr(v, '__module__', '').startswith('gluon')])
+        # create jinja2 env and template
+        request = self.web2py_locals['request']
         app = 'applications.%s' % request.application
         jinja_env = Environment(loader=PackageLoader(app, 'views'))
         template_name = '%s/%s.%s' % (request.controller,
@@ -46,6 +57,7 @@ class Web2Jinja(object):
 
     def __call__(self, func):
         def wrapper(*args, **kwargs):
-            context = func(*args, **kwargs)
+            # create template context
+            context = dict(func(*args, **kwargs).items() + self.web2py_locals.items())
             return self.template.render(**context)
         return wrapper
